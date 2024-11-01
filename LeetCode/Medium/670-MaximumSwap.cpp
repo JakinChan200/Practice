deabc6881048b12dc681e903edd89a80689e3b88
Jakin Chan <JakinChan200@gmail.com>
2024-10-31 23:31:29 -0700

    Completed 670

---
+++ LeetCode/Medium/670-MaximumSwap.cpp
@@ -0,0 +1,61 @@
+//Given an integer num, you can swap two digits at most to get the maximum valued number
+//Return that max value
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printVector(vector<int> digits){
+    for(int i = 0; i < digits.size(); i++){
+        cout << digits[i] << endl;
+    }
+}
+
+vector<int> breakDownNum(int num){
+    vector<int> solution;
+
+    while(num > 0){
+        solution.emplace_back(num%10);
+        num /= 10;
+    }
+
+    reverse(solution.begin(), solution.end());
+    return solution;
+}
+
+int buildNum(vector<int> digits){
+    int solution = 0;
+
+    for(int i = 0; i < digits.size(); i++){
+        solution *= 10;
+        solution += digits[i];
+    }
+
+    return solution;
+}
+
+int maximumSwap(int num){
+    vector<int> digits = breakDownNum(num);
+    int indexOfMax = -1;
+    int left = -1;
+    int right = -1;
+
+    for(int i = digits.size()-2; i >= 0; i--){
+        if(digits[i] > digits[indexOfMax]){
+            indexOfMax = i;
+        }else if(digits[i] < digits[indexOfMax]){
+            left = i;
+            right = indexOfMax;
+        }
+    }
+    if(left == -1) return num;
+    swap(digits[left], digits[right]);
+    return buildNum(digits);
+}
+
+int main(int argc, char** argv){
+    int num = 2736;
+
+    cout << maximumSwap(num) << endl;
+    return 0;
+}
