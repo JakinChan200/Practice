a9340bb410b987b402971edc1bcf915a0a04fc66
Jakin Chan <JakinChan200@gmail.com>
2025-01-21 16:35:35 -0800

    167

---
+++ LeetCode/Medium/167-TwoSumII-InputArrayIsSorted.cpp
@@ -0,0 +1,8 @@
+//167
+//Given an array of ascending numbers, return the index of the two numbers that result in the sum
+//Cannot use the same element twice
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
