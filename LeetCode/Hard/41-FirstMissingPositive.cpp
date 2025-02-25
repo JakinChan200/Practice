fba6f8cdb84125bb62ca030312c548915ab4c520
Jakin Chan <JakinChan200@gmail.com>
2025-02-24 22:11:17 -0800

    41

---
+++ LeetCode/Hard/41-FirstMissingPositive.cpp
@@ -0,0 +1,18 @@
+//41
+//Given an unsorted integer array nums, return the smallest positive integer that is not present in nums
+//O(n) time and O(1) space
+
+#include <vector>
+
+using namespace std;
+
+int firstMissingPositive(vector<int>& nums) {
+    int n = nums.size(); 
+    for (int i = 0; i < n; i++)
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+            swap(nums[i], nums[nums[i] - 1]);
+    for (int i = 0; i < n; i++)
+        if (nums[i] != i + 1)
+            return i + 1;
+    return n + 1;
+}
